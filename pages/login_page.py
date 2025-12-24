fcb2eb7bea07afc1dbb90f67c51e0ae79a5effc2
Akshay Bali <akshaybali@Akshays-MacBook-Air.local>
2025-12-24 17:19:00 +0530

    text alignment

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,27 +8,25 @@ from pages.base_page import BasePage
 
 class LoginPage(BasePage):
 
-
-
-    def __init__(self,driver):
+    def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
         self.username_field = (By.ID, "user-name")
         self.password_field = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
-        self.error_text = (By.CSS_SELECTOR,".error-message-container.error>h3")
+        self.error_text = (By.CSS_SELECTOR, ".error-message-container.error>h3")
 
-    def do_login(self,url,username,password):
+    def do_login(self, url, username, password):
         self.driver.get(url)
         print(f"Login in with username: {username} ")
         # self.driver.find_element(*self.username_field).send_keys(username)
         # self.driver.find_element(*self.password_field).send_keys(password)
         # self.driver.find_element(*self.login_button).click()
-        self.enter_text(self.username_field,username)
+        self.enter_text(self.username_field, username)
         self.enter_text(self.password_field, password)
-        self.click(self.login_button )
+        self.click(self.login_button)
         try:
-            wait = WebDriverWait(self.driver,5)
+            wait = WebDriverWait(self.driver, 5)
             wait.until(EC.url_contains("inventory.html"))
             return True
         except Exception as err:
@@ -46,7 +44,7 @@ class LoginPage(BasePage):
         try:
             wait = WebDriverWait(self.driver, 15)
             wait.until(EC.visibility_of_element_located(self.error_text))
-            assert "Sorry" in self.driver.find_element(*self.error_text).text,"Error message is not shown"
+            assert "Sorry" in self.driver.find_element(*self.error_text).text, "Error message is not shown"
             return "fail"
         except Exception as err:
             return "pass"
