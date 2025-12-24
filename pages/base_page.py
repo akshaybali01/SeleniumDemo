61dd7a9aca4a64f7d041923d94f254dd3f9daf39
Akshay Bali <akshaybali@Akshays-MacBook-Air.local>
2025-12-24 19:39:20 +0530

    implemented logger

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,12 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from utils.logger_util import create_logger
+
+
+
 class BasePage:
     def __init__(self,driver):
         self.driver=driver
+        self.logger = create_logger(self.__class__.__name__)
 
     def click(self,locator):
+        self.logger.info(f"Clicking on {locator}")
         self.driver.find_element(*locator).click()
 
     def enter_text(self,locator,text):
+        self.logger.info(f"Entering {text} in {locator}")
         self.driver.find_element(*locator).send_keys(text)
---
+++ pages/logout_page.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+
+from pages.base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class LogoutPage(BasePage):
+
+    def __init__(self,driver):
+        super().__init__(driver)
+        self.wait = WebDriverWait(driver,10)
+        self.hamburger_menu=(By.ID,"react-burger-menu-btn")
+        self.logout_link = (By.ID,"logout_sidebar_link")
+        self.home_url = "https://www.saucedemo.com"
+
+    def do_logout(self):
+        self.wait.until(EC.visibility_of_element_located(self.hamburger_menu))
+        self.click(self.hamburger_menu)
+        self.wait.until(EC.visibility_of_element_located(self.logout_link))
+        self.click(self.logout_link)
+        return self.driver.current_url == self.home_url
+
+
+
+
