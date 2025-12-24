f2ec74dad6ab68d7460e9c894cc6d85e6f961944
Akshay Bali <akshaybali@Akshays-MacBook-Air.local>
2025-12-24 19:47:55 +0530

    deleted temp function

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -2,14 +2,9 @@ from pages.logout_page import LogoutPage
 from utils.logger_util import create_logger
 from pathlib import Path
 
+
 def test_logout(logged_in_driver):
-    driver,login_success = logged_in_driver
-    assert login_success,"Login failed, Stoping the test execution"
+    driver, login_success = logged_in_driver
+    assert login_success, "Login failed, Stoping the test execution"
     logout_page = LogoutPage(driver)
     logout_page.do_logout()
-
-
-def test_temp():
-    logger = create_logger(str(Path(__file__)))
-    logger.info("This is an info message")
-    logger.debug("This is a debug message")
